b105af2b4777957f24ad533c3b74e708642d7b36
unknown <ahmed.ezzat119@outlook.com>
2020-04-10 18:54:30 +0200

    0

--- auto.py
+++ auto.py
@@ -3,5 +3,5 @@ import os
 
 for i in range(1337):
     os.system("git add .")
-    open("data.bin","w").write(str(random.randint(0,999999999999)))
+    open("data.bin.py","w").write(str(random.randint(0,999999999999)))
     os.system('git commit -m "%s"' % i)
