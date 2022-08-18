376a3d9ac25485c1db318966315bbded2771d565
Ahmed Ezzat (BitTheByte) <ahmed.ezzat119@outlook.com>
2022-08-19 01:30:14 +0200

    0

--- auto.py
+++ auto.py
@@ -1,7 +1,9 @@
 import random
 import os
 
-for i in range(1337 + 1):
+i = 0
+while True:
     os.system("git add .")
     open("data.bin.py","w").write(str(random.randint(0,999999999999)))
     os.system('git commit -m "%s"' % i)
+    i += 1
