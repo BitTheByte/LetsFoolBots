5de883597a78b9dcff81d0e3b20e043ede79576b
Sourcery AI <>
2023-06-04 18:53:09 +0000

    'Refactored by Sourcery'

--- auto.py
+++ auto.py
@@ -5,5 +5,5 @@ i = 0
 while True:
     os.system("git add .")
     open("data.bin.py","w").write(str(random.randint(0,999999999999)))
-    os.system('git commit -m "%s"' % i)
+    os.system(f'git commit -m "{i}"')
     i += 1
